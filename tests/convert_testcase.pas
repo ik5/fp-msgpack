6f56c17b1b2df52ce3f6d7a768ec9a3a7358f984
ik5 <idok@linesip.com>
2012-10-10 13:18:09 +0200

    Moved into Pascal's unit tests intead of my own

---
+++ tests/convert_testcase.pas
@@ -0,0 +1,58 @@
+{ Basic msgpack unit test for converter unit
+
+  Copyright (c) 2012 Ido Kaner <idokan@gmail.com>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to
+  deal in the Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+  sell copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE.
+}
+unit convert_testcase;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpcunit, testutils, testregistry;
+
+type
+
+  TConvertTest= class(TTestCase)
+  protected
+    procedure SetUp; override;
+  published
+    procedure TestHookUp;
+  end;
+
+implementation
+
+procedure TConvertTest.TestHookUp;
+begin
+  Fail('Write your own test');
+end;
+
+procedure TConvertTest.SetUp;
+begin
+
+end;
+
+
+initialization
+
+  RegisterTest(TConvertTest);
+end.
+
