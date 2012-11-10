da0b15ce4153fb65ca5248ac338744f645256f08
ik5 <idok@linesip.com>
2012-11-10 02:58:39 +0200

    The spec said that we need to store the length as well in 4 byte value. Need to test it further with other implementation

--- src/msgpack.pas
+++ src/msgpack.pas
@@ -256,7 +256,7 @@ function TMsgPackRaw.AsUCS4Char: UCS4Char;
 begin
   if FRawData[0] = notRaw32 then
     begin
-      Move(FRawData[1], Result, SizeOf(Result));
+      Move(FRawData[2], Result, SizeOf(Result));
       Result := BEtoN(Result);
     end
   else raise EMsgPackWrongType.Create(errRawSizeTooBig);
@@ -338,10 +338,11 @@ end;
 procedure TMsgPackRaw.Value(AValue: UCS4Char);
 var ConvertedValue : UCS4Char;
 begin
-  SetLength(FRawData, SizeOf(AValue) +1);
+  SetLength(FRawData, SizeOf(AValue) +2);
   FRawData[0]    := notRaw32;
+  FRawData[1]    := SizeOf(AValue);
   ConvertedValue := NtoBE(AValue);
-  Move(ConvertedValue, FRawData[1], SizeOf(AValue));
+  Move(ConvertedValue, FRawData[2], SizeOf(AValue));
 end;
 
 procedure TMsgPackRaw.Value(AValue: ShortString);
